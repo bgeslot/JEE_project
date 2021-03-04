bdb7ef0f3bd9905325ac75d0de047a882165a2aa
95gas <46025764+95gas@users.noreply.github.com>
2021-03-04 19:03:45 +0100

    Sorted out home-page

--- MUSIQUA/js/script.js
+++ MUSIQUA/js/script.js
@@ -1,19 +1,19 @@
+
+<!-- ==================================================== -->
+<!-- ==================  ANIMATION  ===================== -->
+<!-- ==================================================== -->
 (function($) {
-    "use strict";
 
     // Windows load
-
     $(window).on("load", function() {
-
-        // Site loader
-
+        // Site loader Icon
+        
         $(".loader-inner").fadeOut();
         $(".loader").delay(200).fadeOut("slow");
-
     });
 
 
-    // Site navigation setup
+    // Fade the header while we scroll down the page
 
     var header = $('.header'),
         pos = header.offset(),
@@ -30,65 +30,6 @@
                 $(this).removeClass('switched-header').addClass('default').fadeIn(100);
                 blockTop.removeClass('active');
             });
-        }
-    });
-
-
-
-
-    //welcome resize
-    var mainHero = $(" .welcome");
-    function mainHeroResize() {
-        mainHero.css('height', $(window).height());
-    }
-
-    $(function() {
-            mainHeroResize()
-        }),
-        $(window).resize(function() {
-            mainHeroResize()
-        });
-
-
-
-    // Mobile menu
-
-    var mobileBtn = $('.mobile-but');
-    var nav = $('.main-nav ul.main-menu');
-    var navHeight = nav.height();
-
-    $(mobileBtn).on("click", function() {
-        $(".toggle-mobile-but").toggleClass("active");
-        nav.slideToggle();
-        $('.main-nav li a').addClass('mobile');
-        return false;
-
-
-    });
-
-
-
-    $(window).resize(function() {
-        var w = $(window).width();
-        if (w > 320 && nav.is(':hidden')) {
-            nav.removeAttr('style');
-            $('.main-nav li a').removeClass('mobile');
-        }
-
-    });
-
-
-    // Tabbed content
-
-    $(".block-tabs li").on("click", function() {
-        if (!$(this).hasClass("active")) {
-            var tabNum = $(this).index();
-            var nthChild = tabNum + 1;
-            $(".block-tabs li.active").removeClass("active");
-            $(this).addClass("active");
-            $(".block-tab li.active").removeClass("active");
-            $(".block-tab li:nth-child(" + nthChild + ")").addClass("active");
-
         }
     });
 
