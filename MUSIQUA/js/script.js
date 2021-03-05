f1ae9e3dd41fc2bc05ab6ceac41fa96277745363
95gas <46025764+95gas@users.noreply.github.com>
2021-03-05 18:06:45 +0100

    css again

--- MUSIQUA/js/script.js
+++ MUSIQUA/js/script.js
@@ -16,19 +16,16 @@
     // Fade the header while we scroll down the page
 
     var header = $('.header'),
-        pos = header.offset(),
-        blockTop = $('.block-top');
+        pos = header.offset();
 
     $(window).scroll(function() {
         if ($(this).scrollTop() > pos.top + 500 && header.hasClass('default')) {
             header.fadeOut('fast', function() {
                 $(this).removeClass('default').addClass('switched-header').fadeIn(200);
-                blockTop.addClass('active');
             });
         } else if ($(this).scrollTop() <= pos.top + 500 && header.hasClass('switched-header')) {
             header.fadeOut('fast', function() {
                 $(this).removeClass('switched-header').addClass('default').fadeIn(100);
-                blockTop.removeClass('active');
             });
         }
     });
